f72f0cc806ed2580c299818c38b14d3253224601
Leo Guo <shxguozhq@qq.com>
2023-03-21 14:22:26 +0800

    1.完成RedirectToWelcome1页面 2.添加4个欢迎页面的路由

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { RedirectToWelcome1 } from './components/RedirectToWelcome1'
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <div>Hello world!</div>,
+    element: <div>Hello world!<Outlet /></div>,
+    errorElement: <RedirectToWelcome1 />,
+    children: [
+      { index: true, element: <div>请选择 1 2 3</div> },
+      {
+        path: 'welcome',
+        element: <div><Outlet /></div>,
+        children: [
+          { index: true, element: <div></div> },
+          { path: '1', element: <div>1</div> },
+          { path: '2', element: <div>2</div> },
+          { path: '3', element: <div>3</div> },
+          { path: '4', element: <div>4</div> }
+        ]
+      },
+    ]
   },
 ])
 
