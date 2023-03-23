7ba35083c2773b9568e8b61d7f6df59174aaf6d4
Leo Guo <shxguozhq@qq.com>
2023-03-23 16:40:19 +0800

    引入zustand 封装useLocalStore Hook

---
+++ src/stores/useLocalStore.tsx
@@ -0,0 +1,15 @@
+import create from 'zustand'
+
+interface Local {
+  hasReadWelcomes: boolean
+  sethasReadWelcomes: (read: boolean) => void
+}
+const init = localStorage.getItem('hasReadWelcomes')
+export const useLocalStore = create<Local>(set => ({
+  hasReadWelcomes: init === 'yes',
+  sethasReadWelcomes: (read: boolean) => {
+    const result = read ? 'yes' : 'no'
+    localStorage.setItem('hasReadWelcomes', result)
+    set({ hasReadWelcomes: result === 'yes' })
+  }
+}))
