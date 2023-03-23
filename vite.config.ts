97294d35ccdea30a51263307e7a48c746f27976c
Leo Guo <shxguozhq@qq.com>
2023-03-23 23:23:23 +0800

    使用 vite-plugin-mock 在本地创建mock服务器

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import UnoCSS from 'unocss/vite'
+import { viteMockServe } from 'vite-plugin-mock'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [UnoCSS({}), react()]
-})
+export default defineConfig(({ command }) => ({
+  plugins: [
+    UnoCSS({}),
+    react(),
+    viteMockServe({
+      mockPath: 'mock',
+      localEnabled: command === 'serve',
+    }),
+  ]
+}))
