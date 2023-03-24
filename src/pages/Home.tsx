5a2c067265a463e8cb280debe6a7078e63e51a6c
Leo Guo <shxguozhq@qq.com>
2023-03-24 14:59:32 +0800

    新建三个组件

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,9 @@
 import useSWR from 'swr'
 import { Navigate } from 'react-router-dom'
 import pot from '../assets/images/pot.svg'
-import add from '../assets/icons/add.svg'
 import { ajax } from '../lib/ajax'
+import { AddItemFloatButton } from '../components/AddItemFloatButton'
+import { Loading } from '../components/Loading'
 export const Home: React.FC = () => {
   const { data: meData, isLoading: isLoadingMe }
   = useSWR('/api/v1/me', async path => (
@@ -13,7 +14,7 @@ export const Home: React.FC = () => {
      (await ajax.get<Resources<Item>>(path)).data)
 
   if (isLoadingMe || isLoadingItems) {
-    return <div>加载中。。。</div>
+    return <Loading />
   }
   if (itemsData?.resources[0]) {
     return <Navigate to="/items"/>
@@ -27,9 +28,6 @@ export const Home: React.FC = () => {
         rounded-8px
       >开始记账</button>
     </div>
-    <button p-4px w-56px h-56px bg="#5C33BE" rounded="50%" b-none text-white
-      text-6xl fixed bottom-16px right-16px>
-      <img src={add} max-w="100%" max-h="100%" />
-    </button>
+    <AddItemFloatButton />
   </div>
 }
