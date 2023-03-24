ce39046dad24f4c45d6f9d8925c10cde8c766a77
Leo Guo <shxguozhq@qq.com>
2023-03-24 14:20:17 +0800

    修复 mock bug，添加远程数据类型，http.get 接受泛型，完善 Home 页面展示逻辑

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,23 @@
-import axios from 'axios'
 import useSWR from 'swr'
+import { Navigate } from 'react-router-dom'
 import pot from '../assets/images/pot.svg'
 import add from '../assets/icons/add.svg'
+import { ajax } from '../lib/ajax'
 export const Home: React.FC = () => {
-  const { data: meData, error: meError } = useSWR('/api/v1/me', (path) => {
-    return axios.get(path)
-  })
-  const { data: itemsData, error: itemsError } = useSWR(meData ? '/api/v1/items' : null, (path) => {
-    return axios.get(path)
-  })
-  console.log(meData, meError, itemsData, itemsError)
+  const { data: meData, isLoading: isLoadingMe }
+  = useSWR('/api/v1/me', async path => (
+    await ajax.get<Resource<User>>(path)).data.resource
+  )
+  const { data: itemsData, isLoading: isLoadingItems }
+   = useSWR(meData ? '/api/v1/items' : null, async path =>
+     (await ajax.get<Resources<Item>>(path)).data)
+
+  if (isLoadingMe || isLoadingItems) {
+    return <div>加载中。。。</div>
+  }
+  if (itemsData?.resources[0]) {
+    return <Navigate to="/items"/>
+  }
   return <div>
     <div flex justify-center items-center>
       <img mt-20vh mb-20vh width="128" height="130" src={pot} />
