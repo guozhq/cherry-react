05a45d8e7822e55e908de41f58f1755aac89a870
Leo Guo <shxguozhq@qq.com>
2023-03-27 15:06:47 +0800

    创建登录页面

--- src/pages/ItemsPage.tsx
+++ src/pages/ItemsPage.tsx
@@ -1,26 +1,26 @@
 import { useState } from 'react'
-import styled from 'styled-components'
 import { AddItemFloatButton } from '../components/AddItemFloatButton'
 import type { TimeRange } from '../components/TimeRangePicker'
 import { TimeRangePicker } from '../components/TimeRangePicker'
 import { TopNav } from '../components/TopNav'
 import { TopMenu } from '../components/TopMenu'
 import { useMenuStore } from '../stores/useMenuStore'
+import { Gradient } from '../components/Gradient'
+import { Icon } from '../components/Icon'
 import { ItemsList } from './ItemsPage/ItemsList'
 import { ItemsSummary } from './ItemsPage/ItemsSummary'
-const Div = styled.div`
-background: linear-gradient(0deg, rgba(143,76,215,1) 0%, rgba(92,51,190,1) 100%);
-`
 
 export const ItemsPages: React.FC = () => {
   const [timeRange, setTimeRange] = useState<TimeRange>('thisMonth')
   const { visible, setVisible } = useMenuStore()
   return (
     <div>
-      <Div>
-        <TopNav />
+      <Gradient>
+        <TopNav title="账目列表" icon={
+          <Icon name="menu" className="w-24px h-24px" onClick={() => { setVisible(!visible) }} />
+        } />
         <TimeRangePicker selected={timeRange} onSelected={setTimeRange} />
-      </Div>
+      </Gradient>
       <ItemsSummary />
       <ItemsList />
       <AddItemFloatButton />
