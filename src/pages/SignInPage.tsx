71fa62b7a7bdd70ec0ddcd60dfd1ddaaa71c7da8
Leo Guo <shxguozhq@qq.com>
2023-03-28 11:24:42 +0800

    添加表单校验逻辑

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -2,13 +2,18 @@ import type { FormEventHandler } from 'react'
 import { Gradient } from '../components/Gradient'
 import { Icon } from '../components/Icon'
 import { TopNav } from '../components/TopNav'
+import { validate } from '../lib/validate'
 import { useSignInStore } from '../stores/useSignInStore'
 
 export const SignInPage: React.FC = () => {
-  const { data, setData } = useSignInStore()
+  const { data, error, setData, setError } = useSignInStore()
   const onSubmit: FormEventHandler<HTMLFormElement> = (e) => {
     e.preventDefault()
-    console.log(data)
+    const error = validate(data, [
+      { key: 'email', type: 'required', message: '请输入邮箱' },
+      { key: 'code', type: 'required', message: '请输入验证码' },
+    ])
+    setError(error)
   }
   return (<div>
     <Gradient>
@@ -21,14 +26,14 @@ export const SignInPage: React.FC = () => {
       <form j-form onSubmit={onSubmit}>
       <div b-1 b-red>{JSON.stringify(data)}</div>
         <div>
-          <span j-form-label>邮箱地址</span>
+          <span j-form-label>邮箱地址{ error.email?.[0] && <span>{error.email[0]}</span>}</span>
           <input j-input-text type="text"
           placeholder='请输入邮箱，然后点击发送验证码'
           value={data.email} onChange={e => setData({ email: e.target.value })}
           />
         </div>
         <div>
-          <span j-form-label>验证码</span>
+          <span j-form-label>验证码{ error.code?.[0] && <span>{error.code[0]}</span>}</span>
           <div flex gap-x-16px>
             <input j-input-text type="text" placeholder='六位数字'
             value={data.code} onChange={e => setData({ code: e.target.value })}/>
