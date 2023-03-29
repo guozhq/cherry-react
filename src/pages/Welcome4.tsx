2e7d787300fe86ea20994bb933d7053de354cd42
Leo Guo <shxguozhq@qq.com>
2023-03-29 13:01:14 +0800

    点击跳过或者开启应用时，记录用户已经读过广告

--- src/pages/Welcome4.tsx
+++ src/pages/Welcome4.tsx
@@ -1,6 +1,13 @@
-import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import cloud from '../assets/images/cloud.svg'
+import { useLocalStore } from '../stores/useLocalStore'
 export const Welcome4: React.FC = () => {
+  const { sethasReadWelcomes } = useLocalStore()
+  const nav = useNavigate()
+  const onSkip = () => {
+    sethasReadWelcomes(true)
+    nav('/welcome/xxx')
+  }
   return (
     <div>
       <img w-129px h-83px src={cloud} />
@@ -9,7 +16,7 @@ export const Welcome4: React.FC = () => {
         再也不怕数据丢失
       </h2>
       <div mt-64px>
-        <Link text-32px color="#6035BF" font-bold to="/home">开启应用</Link>
+        <span text-32px color="#6035BF" font-bold onClick={onSkip}>开启应用</span>
       </div>
     </div>)
 }
