366478a62d9fe446eb64d1afb73bec176e632dbd
Leo Guo <shxguozhq@qq.com>
2023-03-31 11:23:33 +0800

    Tabs 支持自定义三处的样式

--- src/pages/ItemsNewPage.tsx
+++ src/pages/ItemsNewPage.tsx
@@ -1,13 +1,25 @@
+import type { ReactNode } from 'react'
+import { useState } from 'react'
 import { Gradient } from '../components/Gradient'
 import { Icon } from '../components/Icon'
+import { Tabs } from '../components/Tabs'
 import { TopNav } from '../components/TopNav'
+import s from './ItemsNewPage.module.scss'
 
+type ItemKind = 'income' | 'expenses'
 export const ItemsNewPages: React.FC = () => {
+  const tabItems: { key: ItemKind; text: string; element: ReactNode }[]
+    = [{ key: 'expenses', text: '支出', element: <div>支出</div> },
+      { key: 'income', text: '收入', element: <div>收入</div> }]
+  const [tabItem, setTabItem] = useState<ItemKind>('expenses')
   return (
-    <div>
+    <div className={s.wrapper}>
       <Gradient>
         <TopNav title="记一笔" icon={<Icon name="back" />} />
       </Gradient>
+      <Tabs tabItems={tabItems} className="text-center" classPrefix='itemsNewTabs'
+          value={tabItem}
+          onChange={(item) => { setTabItem(item) }} />
     </div>
   )
 }
