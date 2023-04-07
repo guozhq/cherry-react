6975edd6fd11eebe6a16da7d9a758ae219116fcd
Leo Guo <shxguozhq@qq.com>
2023-04-07 12:45:32 +0800

    删除 ajax 导出，只使用 useAjax

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,46 +1,43 @@
 import useSWR from 'swr'
-import { Navigate, useNavigate } from 'react-router-dom'
-import type { AxiosError } from 'axios'
-import pot from '../assets/images/pot.svg'
-import { ajax } from '../lib/ajax'
-import { AddItemFloatButton } from '../components/AddItemFloatButton'
-import { Loading } from '../components/Loading'
+import { Navigate } from 'react-router-dom'
+import p from '../assets/images/pig.svg'
+import { useAjax } from '../lib/ajax'
 import { useTitle } from '../hooks/useTitle'
+import { Loading } from '../components/Loading'
+import { AddItemFloatButton } from '../components/AddItemFloatButton'
 interface Props {
   title?: string
 }
 export const Home: React.FC<Props> = (props) => {
   useTitle(props.title)
-  const nav = useNavigate()
-  const onHttpError = (error: AxiosError) => {
-    if (error.response) {
-      if (error.response.status === 401) {
-        nav('/sign_in')
-      }
-    }
-    throw error
-  }
-  const { data: meData, isLoading: isLoadingMe }
-  = useSWR('/api/v1/me', async path => (
-    await ajax.get<Resource<User>>(path)).data.resource
+  const { get } = useAjax({ showLoading: true, handleError: false })
+  const { data: meData, error: meError } = useSWR('/api/v1/me', async path => {
+    // 如果返回 403 就让用户先登录
+    const response = await get<Resource<User>>(path)
+    return response.data.resource
+  })
+  const { data: itemsData, error: itemsError } = useSWR(meData ? '/api/v1/items' : null, async path =>
+    (await get<Resources<Item>>(path)).data
   )
-  const { data: itemsData, isLoading: isLoadingItems }
-   = useSWR(meData ? '/api/v1/items' : null, async path =>
-     (await ajax.get<Resources<Item>>(path)).data)
+
+  const isLoadingMe = !meData && !meError
+  const isLoadingItems = meData && !itemsData && !itemsError
 
   if (isLoadingMe || isLoadingItems) {
-    return <Loading className="h-screen" message='正在加载页面，请稍等'/>
+    return <Loading className="h-screen" />
   }
+
   if (itemsData?.resources[0]) {
-    return <Navigate to="/items"/>
+    return <Navigate to="/items" />
   }
+
   return <div>
     <div flex justify-center items-center>
-      <img mt-20vh mb-20vh width="128" height="130" src={pot} />
+      <img mt-20vh mb-20vh width="128" height="130" src={p} />
     </div>
     <div px-16px>
-    <button j-btn>开始记账</button>
+      <button j-btn>开始记账</button>
     </div>
     <AddItemFloatButton />
-  </div>
+  </div >
 }
