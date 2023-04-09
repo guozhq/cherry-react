d06695eaa4efb20eb4503a54d57a4c00f9aa77ad
Leo Guo <shxguozhq@qq.com>
2023-04-09 09:28:24 +0800

    登录页可以跳回 from 页面

--- src/pages/ItemsPageError.tsx
+++ src/pages/ItemsPageError.tsx
@@ -1,10 +1,12 @@
-import { Navigate, useRouteError } from 'react-router-dom'
+import { Navigate, useLocation, useRouteError } from 'react-router-dom'
 import { ErrorEmptyData, ErrorUnauthorized } from '../errors'
 
 export const ItemsPageError: React.FC = () => {
   const error = useRouteError() as Error
+  const loc = useLocation()
   if (error instanceof ErrorUnauthorized) {
-    return <Navigate to="/sign_in" />
+    const from = encodeURIComponent(`${loc.pathname}${loc.search}`)
+    return <Navigate to={`/sign_in?from=${from}`} />
   } else if (error instanceof ErrorEmptyData) {
     return <Navigate replace to="/home" />
   } else {
