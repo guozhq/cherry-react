26188834258e73b01f64d8acc01f575f587e71b4
Leo Guo <shxguozhq@qq.com>
2023-04-14 10:41:00 +0800

    将 ItemDate 从 DateAndAmount 中抽离出来

--- src/pages/ItemsNewPage.tsx
+++ src/pages/ItemsNewPage.tsx
@@ -6,6 +6,7 @@ import { TopNav } from '../components/TopNav'
 import { useCreateItemStore } from '../stores/useCreateItemStore'
 import s from './ItemsNewPage.module.scss'
 import { DateAndAmount } from './ItemsNewPage/DateAndAmount'
+import { ItemDate } from './ItemsNewPage/ItemDate'
 import { Tags } from './ItemsNewPage/Tags'
 
 export const ItemsNewPage: React.FC = () => {
@@ -35,7 +36,7 @@ export const ItemsNewPage: React.FC = () => {
         value={data.kind!}
         onChange={(tabItem) => { setData({ kind: tabItem }) }} />
       <div text-28px>{JSON.stringify(data)}</div>
-          <DateAndAmount className="grow-0 shrink-0" />
+          <DateAndAmount className="grow-0 shrink-0" itemDate={<ItemDate />}/>
     </div>
   )
 }
